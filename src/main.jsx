56d6237491849dd25ba0550317d4d9bef32797e4
jesus_plata <jesusplata1520@gmail.com>
2025-02-19 12:11:08 -0500

    actualizado

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import {CounterWithCustomHook} from './useState/CounterWithCustomHook'
 //import {HookApp} from './HookApp'
-import { Counter } from './useState/Counter'
+//import { Counter } from './useState/Counter'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Counter />
+    <CounterWithCustomHook />
   </StrictMode>,
 )
---
+++ src/useState/CounterWithCustomHook.jsx
@@ -0,0 +1,13 @@
+export const CounterWithCustomHook = () => {
+
+  
+    return(
+        <>
+        <h1>Counter With CustomHook</h1>
+        <hr/>
+        <button className="btn btn-primary">+1</button>
+        <button className="btn btn-primary">+Reset</button>
+        <button className="btn btn-primary">-1</button>
+    </>
+    )
+}
