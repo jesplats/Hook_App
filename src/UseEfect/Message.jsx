07ec5c99731ef2908998db83db9115aac6c5e021
jesus_plata <jesusplata1520@gmail.com>
2025-02-20 10:32:44 -0500

    a

--- src/UseEfect/Message.jsx
+++ src/UseEfect/Message.jsx
@@ -1,10 +1,16 @@
 import { useEffect } from 'react'
+import { useState } from 'react'
 export const Message = () => {
  
+const  [coors, setcoors] = useState({x:0, y:0})
+
+
  useEffect(() => {
-   
+ 
+    
    const onMouseMove = ({x,y}) => {
-    const coors = {x, y}
+   //const coors = {x, y}
+   setcoors({x,y})
     console.log(coors)
    }
    
@@ -19,6 +25,9 @@ export const Message = () => {
     return (
    <>
    <h3> existe</h3>
+   {
+    JSON.stringify(coors)
+   }
    </>
   )
 }
