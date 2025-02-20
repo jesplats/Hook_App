06d4c501020e768a9ccc42f64e6aaab6bcdfd4cc
jesus_plata <jesusplata1520@gmail.com>
2025-02-19 23:55:53 -0500

    ax

--- src/Hook/useCounter.js
+++ src/Hook/useCounter.js
@@ -3,13 +3,14 @@ import { useState } from "react";
 export const UseCounter = (initiValue = 10) => {
   const [counter, setcounter] = useState(initiValue);
 
-  const increment = () => {
-    setcounter(counter + 1);
+  const increment = ({ value = 2 }) => {
+    if (counter == 20) return;
+    setcounter(counter + value);
   };
 
-  const decrementar = () => {
+  const decrementar = ({ value = 1 }) => {
     if (counter == 0) return;
-    setcounter(counter - 1);
+    setcounter(counter - value);
   };
 
   const reset = () => {
